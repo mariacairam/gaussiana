97d7fa1570a83516b5f87de3d292d803643cd75a
Maria Carolina Moreno <63366140+mcmoreno-ufrj@users.noreply.github.com>
2020-06-22 10:30:30 -0300

    Add files via upload

--- testes/gau.py
+++ testes/gau.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy.signal
 from scipy.optimize import curve_fit
+from matplotlib import gridspec
 
 dados = open('dadosfit.log', 'w')
 y_array = open('y_array.txt', 'w')
@@ -35,16 +36,6 @@ def linear(x, a, b):
     return a * x + b
 
 
-aa = 0
-bb = 0
-ix1 = 109
-ix2 = nLinhas - 109
-for i in range(0, 41, 1):
-    aa = aa + yp[109 + i]
-    bb = bb + yp[nLinhas - 109 - i]
-aa = aa / 41
-bb = bb / 41
-
 mqlin = np.zeros(82)
 yplin = np.zeros(82)
 for i in range(0, 41, 1):
@@ -54,10 +45,7 @@ for i in range(0, 41, 1):
     yplin[81 - i] = yp[nLinhas - 109 - i]
 print(yplin)
 
-bcoef = (bb - aa) / (ix2 - ix1)
-acoef = aa - bcoef * ix1
-
-popt_linear, pcov_linear = scipy.optimize.curve_fit(linear, mqlin, yplin, p0=[bcoef, acoef])
+popt_linear, pcov_linear = scipy.optimize.curve_fit(linear, mqlin, yplin)
 
 fig = plt.figure(figsize=(4, 3))
 plt.plot(mq, yp)
@@ -66,17 +54,19 @@ plt.show()
 
 base = linear(mq, *popt_linear)
 yp = yp - base
-yp = scipy.signal.savgol_filter(yp, 11, 2)
+f = scipy.interpolate.interp1d(mq, yp)
+mq = np.arange(mq[0], mq[nLinhas - 1] - 0.1, 0.01)
+yp = f(mq)
+base = linear(mq, *popt_linear)
 
+picos, _ = scipy.signal.find_peaks(yp, height=100 - base, threshold=None, distance=50, width=1.5)
 
-picos, _ = scipy.signal.find_peaks(yp, height=100-base, threshold=None, distance=10, width=1.5)
-
-picos = np.delete(picos, [10, 23])
+picos = np.delete(picos, [10])
 print(picos)
 print(mq[picos])
 print(yp[picos])
 
-fig = plt.figure(figsize=(4, 3))
+plt.figure(figsize=(4, 3))
 plt.title('Picos Espectro')
 plt.plot(mq, yp)
 plt.scatter(mq[picos], yp[picos], color='red', marker='+', label='Picos encontrados')
@@ -106,10 +96,26 @@ sigma = (mq[dire] - mq[esq]) / 2 * np.sqrt(2 * np.log(2))
 cen = mq[picos]
 amp = yp[picos]
 
-chute = np.column_stack((amp, cen, sigma))
-print(chute)
+chute = np.zeros(3 * len(picos))
+boundinf = np.zeros(3 * len(picos))
+boundsup = np.zeros(3 * len(picos))
+c = 0
+i = 0
+while c < (3 * len(picos)) and i < len(picos):
+    chute[c] = amp[i]
+    chute[c + 1] = cen[i]
+    chute[c + 2] = sigma[i]
+    boundinf[c] = -np.inf
+    boundinf[c + 1] = cen[i] - 0.2
+    boundinf[c + 2] = -np.inf
+    boundsup[c] = np.inf
+    boundsup[c + 1] = cen[i] + 0.2
+    boundsup[c + 2] = np.inf
+    c += 3
+    i += 1
 
-popt2, pcov2 = scipy.optimize.curve_fit(gaussiana2, mq, yp, p0=chute)
+popt2, pcov2 = scipy.optimize.curve_fit(gaussiana2, mq, yp, p0=chute,
+                                        bounds=(boundinf, boundsup))
 perr = np.sqrt(np.diag(pcov2))
 
 dados.write('Ajustes picos\n')
@@ -138,8 +144,13 @@ for i in range(0, len(picos), 1):
                 '\nArea relativa: ' + str((area[i] / areatot * 100)) + '\n\n')
 
 fig = plt.figure(figsize=(4, 3))
+gs = gridspec.GridSpec(1, 1)
+ax1 = fig.add_subplot(gs[0])
 plt.plot(mq, yp)
 plt.plot(mq, gaussiana2(mq, *popt2), 'k--')
+pico_gauss = gaussiana1(mq, popt2[39], popt2[40], popt2[41])
+ax1.plot(mq, pico_gauss, "b")
+ax1.fill_between(mq, pico_gauss.min(), pico_gauss, facecolor="blue", alpha=0.5)
 plt.xlabel("x: m/q", family="serif", fontsize=12)
 plt.ylabel("y: icounts", family="serif", fontsize=12)
 plt.tick_params(axis='both', which='major', direction="out", top="on", right="on", bottom="on", length=8, labelsize=8)
